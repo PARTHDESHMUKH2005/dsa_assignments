4657f69682bef097c68194ee7393791c719eb28e
PARTHDESHMUKH2005 <parthdeshmukh036@gmail.com>
2025-11-26 19:14:36 +0530

    Create q5.cpp

---
+++ assignment1/q5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[10][10];
+    int r, c;
+
+    cout << "Enter number of rows: ";
+    cin >> r;
+
+    cout << "Enter number of columns: ";
+    cin >> c;
+
+    cout << "Enter the elements of the matrix:\n";
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    // Sum of each row
+    cout << "\nSum of each row:\n";
+    for(int i = 0; i < r; i++) {
+        int rowSum = 0;
+        for(int j = 0; j < c; j++) {
+            rowSum += arr[i][j];
+        }
+        cout << "Row " << i + 1 << " = " << rowSum << endl;
+    }
+
+    // Sum of each column
+    cout << "\nSum of each column:\n";
+    for(int j = 0; j < c; j++) {
+        int colSum = 0;
+        for(int i = 0; i < r; i++) {
+            colSum += arr[i][j];
+        }
+        cout << "Column " << j + 1 << " = " << colSum << endl;
+    }
+
+    return 0;
+}
