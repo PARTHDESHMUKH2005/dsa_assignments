2f1c1b0e9c9b8cbb9414ff235ae8bdffaca92298
PARTHDESHMUKH2005 <parthdeshmukh036@gmail.com>
2025-11-27 11:52:55 +0530

    Create q4.cpp

---
+++ assignment4/q4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+void firstNonRepeating(string s) {
+    queue<char> q;
+    unordered_map<char, int> freq;
+
+    for (char c : s) {
+        freq[c]++;
+        q.push(c);
+        while (!q.empty() && freq[q.front()] > 1)
+            q.pop();
+
+        if (!q.empty())
+            cout << q.front() << " ";
+        else
+            cout << -1 << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    string s;
+    cout << "Enter string: ";
+    cin >> s;
+    firstNonRepeating(s);
+    return 0;
+}
